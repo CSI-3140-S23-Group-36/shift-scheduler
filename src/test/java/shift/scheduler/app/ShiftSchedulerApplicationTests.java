f0a774321b5a75303e624b9126be9adbd59507d4
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-06-11 21:15:36 -0400

    Initial commit

---
+++ src/test/java/shift/scheduler/app/ShiftSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package shift.scheduler.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
