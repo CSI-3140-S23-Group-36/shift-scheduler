54de1a8eac226253e5dfb4ab66968c35918c4ad7
Steven <60322211+stevenli5@users.noreply.github.com>
2023-07-12 22:11:24 -0400

    Added base login page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,27 +1,44 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import Login from './components/pages/common/login';
 import Error from "./components/pages/common/error";
-import Login from "./components/pages/common/login";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import './components/pages/common/login.css'
 import AvailabilityForm from "./components/pages/employee/availabilityForm";
 import GenerateSchedule from "./components/pages/manager/generateSchedule";
 
-const router = createHashRouter([
-  { path: "*", element: <Error /> },
-  { path: "/login", element: <Login /> },
-  { path: "/availabilities", element: <AvailabilityForm /> },
-  { path: "/generate-schedules", element: <GenerateSchedule /> },
-]);
+// const router = createHashRouter([
+//   { path: "*", element: <Error /> },
+//   { path: "/login", element: <Login /> },
+//   { path: "/availabilities", element: <AvailabilityForm /> },
+//   { path: "/generate-schedules", element: <GenerateSchedule /> },
+// ]);
 
-function App() {
+/* Don't know how to use hash router */
+const App: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const handleLogin = () => {
+  };
+
   return (
-    <div className="App">
-      <div id="page-container">
-        <RouterProvider router={router} />
-      </div>
+    <div>
+      <Login
+        username={username}
+        password={password}
+        onInputChange={handleInputChange}
+        onLogin={handleLogin}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- Front/src/components/pages/common/login.tsx
+++ Front/src/components/pages/common/login.tsx
@@ -1,5 +1,44 @@
-import Page from "../../page";
+import React from 'react';
 
-export default function Login() {
-  return <Page>Login form goes here</Page>;
+interface LoginProps {
+  username: string;
+  password: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onLogin: () => void;
 }
+
+const Login: React.FC<LoginProps> = ({
+  username,
+  password,
+  onInputChange,
+  onLogin,
+}) => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1>Shift Scheduler</h1>
+        <button>Help</button>
+      </div>
+
+      <div className="login-container">
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={onInputChange}
+          name="username"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onInputChange}
+          name="password"
+        />
+        <button onClick={onLogin}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
