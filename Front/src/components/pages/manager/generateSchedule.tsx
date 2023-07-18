4c0805cf0ea6c9f5f63deed13c2df9ded751fbb4
Steven <60322211+stevenli5@users.noreply.github.com>
2023-07-18 14:05:46 -0400

    Added generate schedules page

--- Front/src/components/pages/manager/generateSchedule.tsx
+++ Front/src/components/pages/manager/generateSchedule.tsx
@@ -2,5 +2,39 @@ import Page from "../../page";
 
 import React from "react";
 export default function GenerateSchedule() {
-  return <Page>Schedule generation form goes here</Page>;
+  return <Page>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h2>Week</h2>
+      <input
+        type="date"
+        // value={startDate}
+        style={{ marginBottom: "1rem" }}
+      />
+
+      <h2>Employee Details</h2>
+      <label>Number of employees per hour:</label>
+      <input
+        type="number"
+        // value={Number}
+        style={{ marginBottom: "1rem" }}
+      />
+
+      <h2>Time Range</h2>
+      <label>Start Time:</label>
+      <input
+        type="time"
+        // value={startTime}
+        style={{ marginBottom: "1rem" }}
+      />
+
+      <label>End Time:</label>
+      <input
+        type="time"
+        // value={endTime}
+        style={{ marginBottom: "1rem" }}
+      />
+
+      <button>Generate Schedules</button>
+    </div>
+  </Page>;
 }
