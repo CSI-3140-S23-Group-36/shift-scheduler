98cdcec4ba0664694e450138d8ad6365d797e000
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-07-18 20:16:52 -0400

    Return error if no schedules could be generated

--- Back/app/src/main/java/shift/scheduler/app/controllers/GenerationController.java
+++ Back/app/src/main/java/shift/scheduler/app/controllers/GenerationController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -40,6 +41,14 @@ public class GenerationController {
         }
 
         List<ScheduleForWeek> schedules = scheduleGenerator.generateSchedules(requirements);
+
+        if (schedules.size() == 0)
+        {
+            return ResponseEntity.internalServerError().body(
+                    "No schedules could be generated that meet the provided requirements."
+            );
+        }
+
         String schedulesJSON = null;
 
         try {
